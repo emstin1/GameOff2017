5e5a5d480d07c37cffa3dc947695c93b1d950e1c
Eric Stinger <emstin1@protonmail.com>
2017-11-04 03:30:59 -0400

    tweaked the movement

--- guy.gd
+++ guy.gd
@@ -1,35 +1,30 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
+#variables
 var velocity  = Vector2()
-const MAX_VEL = 300
 var current_spd = 0
 
-func move_left(spd):
-	velocity.x = -spd
-
-func move_right(spd):
-	velocity.x = spd
-	
-func is_moving():
-	return (Input.is_action_pressed("player_left")) or (Input.is_action_pressed("player_right"))
+#constants
+const MAX_VEL = 300
+const MV_LAG  = 50
 
 func _process(delta):
-	
-	
+	# the movement code below gives us a bit of a lag effect
+	# when chaning directions or starting from a dead stop.
 	if (Input.is_action_pressed("player_left")):
-		if not current_spd == MAX_VEL:
-			current_spd += 50
-		velocity.x = -current_spd
+		if not current_spd == -MAX_VEL:
+			current_spd -= MV_LAG
+		velocity.x = current_spd
 	elif (Input.is_action_pressed("player_right")):
 		if not current_spd == MAX_VEL:
-			current_spd += 100
+			current_spd += MV_LAG
 		velocity.x = current_spd
 	else:
-		current_spd = 0
+		while current_spd != 0:
+			if current_spd < 0:
+				current_spd += MV_LAG
+			elif current_spd > 0:
+				current_spd -= MV_LAG
 		velocity.x = 0
 
 	move(velocity * delta)
